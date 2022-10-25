99ced9fcd8868bfd25028cf95c023744c7b90d62
Tinouert Alexandre <alexandretinouert@gmail.com>
2022-10-25 11:37:46 +0200

    Added comments and .bat executer
    Added comments and .bat executer

--- main.py
+++ main.py
@@ -1,33 +1,66 @@
-#To properly use this program to visualize the set of data, please find them here: https://1drv.ms/u/s!Ah3H11Zn7vFXgcV1ZtpBQygZ8jSusw?e=SE4IxR
-#And place the /data folder aside with the main.py program.
+'''main.py'''
+'''For easier use, please execute the DoodleRec.bat file provided with this, or read the README file'''
 
-import os
-import json
-from flask import *
-import numpy as np
+import os #To open the files needed for the Flask application
+import json #To receive the JSON data from the JavaScript requests
+from flask import * #For the web application
+import numpy as np #For testing datasets and open the data base that are .npy files
 
+
+#Initialize the Flask applications with the static files
 app = Flask(__name__)
 
 path_cwd = os.path.dirname(os.path.realpath(__file__))
 path_templates = os.path.join(path_cwd,"templates")
 path_static = os.path.join(path_cwd,"static")
 
+
+#Initialize the user's matrix from imported data, which is a 28x28 matrix
+usermatrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+#Render the main page from the index.html static
 @app.route('/')
 def hello():
     return render_template('index.html')
 
+#Receive the data from the JavaScript / JSON request and parse it into the (user)matrix, then print it into the console
 @app.route("/", methods = ["post"])
 def printer():
   userdata = request.get_json()
-  print(userdata["userinput"])
-  return userdata
+  userinput = userdata['userinput']
+  for i in range(0, 28):
+    for j in range(0, 28):
+      usermatrix[i][j] = userinput[28*i+j]
+  print(usermatrix)
+  return usermatrix
 
-A = np.load("data/cookie.npy", allow_pickle=True)
-c = len(A)
-a = A[0]
-B = np.reshape(a, (28,28))
-"""print(B)
-print(c)"""
-
-
+#Run the application
 app.run()
--- static/script.js
+++ static/script.js
@@ -1,25 +1,30 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+/*script.js
+For easier use, please execute the DoodleRec.bat file provided with this, or read the README file*/
 
-let painting = false;
+const canvas = document.getElementById("canvas"); //Create a canvas element
+const ctx = canvas.getContext("2d"); //As well as a context zone for intercation
 
+let painting = false; //Initialize painting state
+
+//Initialize starting position to avoid errors with the user's mouse position
 function startPos(e) {
     painting = true;
     draw(e);
 }
 
+function finPos() {
+  painting = false;
+  ctx.beginPath();
+}
+
+//Initialize canvas' size and context zone to be 28x28 pixels rendered into 420x420px
 canvas.width = 28;
 canvas.height = 28;
 canvas.style.width = "420px";
 canvas.style.height = "420px";
-
-function finPos() {
-    painting = false;
-    ctx.beginPath();
-}
-
 ctx.scale((28/420), (28/420));
 
+//draw function with event listeners
 function draw(e) {
     if (!painting) return;
     ctx.lineWidth = 15;
@@ -35,7 +40,8 @@ canvas.addEventListener('mousedown', startPos);
 canvas.addEventListener('mouseup', finPos);
 canvas.addEventListener('mousemove', draw);
 
-
+//Every 0.5s, this function takes the image data of the input image and returns an array of it
+//It then parses it to a JSON request, that will be fetched by the Flask application
 setInterval(function(){ 
     imgData = ctx.getImageData(0,0,28,28);
     imgDataBW = [];
@@ -44,12 +50,10 @@ setInterval(function(){
         imgDataBW.push(imgData.data[i]);
         document.getElementById("dataCont").innerHTML=imgDataBW;
     }
-    var sendData = {
 
+    var sendData = {
         "userinput" : imgDataBW
         }
-        
-        
           fetch(`${window.origin}/`, {
             method: "POST",
             credentials: "include",
